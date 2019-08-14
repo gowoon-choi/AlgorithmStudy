ce351ab185b6f90bad3b6160998356a63332de6c
choigone <gowoonvv@gmail.com>
2019-08-14 17:31:39 +0900

    new problem

---
+++ SWExpertAcademy/Level-D4/makeGroup.cpp
@@ -0,0 +1,39 @@
+/*
+ * swExpert #8104
+ * makeGroup.cpp
+ * 2019.08.05
+ * 최고운 github.com/choigone
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int testCase;
+    int n,k;
+    int result;
+    vector<int> answer;
+    cin >> testCase;
+    for(int i=0;i<testCase;i++){
+        cin >> n >> k;
+        result = 0;
+        if(n%2==0){
+            result = 1+(n*k);
+            result = result * (n/2);
+        }
+        else{
+            result = 1 + ((n*k)-k+1);
+            result = (result * (n/2)) + (result/2);
+        }
+        answer.push_back(result);
+    }
+    for(int i=0;i<testCase;i++){
+        cout << "#" << i+1;
+        for(int j=0;j<k;j++){
+            cout << " " << result;
+        }
+    }
+    return 0;
+}
