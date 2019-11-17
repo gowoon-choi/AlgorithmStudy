2a115a6665490d20291d565db08be39a400873a4
choigone <gowoonvv@gmail.com>
2019-11-17 00:03:49 -0500

    solve boj10989

---
+++ baekjoon/BOJ10989.cpp
@@ -0,0 +1,34 @@
+/*
+ * baekjoon #2751 수 정렬하기3
+ * BO10989.cpp
+ * 2019.11.16
+ * 최고운 github.com/choigone
+ */
+
+#include <iostream>
+
+#define N 10001
+
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    int arr[N] = {0,};
+    int temp;
+    for(int i=0; i<n; i++){
+        scanf("%d",&temp);
+        arr[temp]++;
+    }
+
+    for(int i=0; i<N; i++){
+        if(arr[N]!=0){
+            for(int j=0; j<arr[i]; j++){
+                printf("%d\n",i);
+            }
+        }
+    }
+
+    return 0;
+}
