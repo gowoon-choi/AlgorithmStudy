92b87dd2e029e6431210ccb40a61518cc2488545
ChoiGoun <gowoonvv@gmail.com>
2020-02-09 23:21:20 +0900

    Create BOJ10869.py

---
+++ baekjoon/python/BOJ10869.py
@@ -0,0 +1,13 @@
+"""
+baekjoon #10869 사칙연산
+BOJ10869.py
+2020.02.09
+최고운 github.com/choigone
+"""
+
+num1, num2 = map(int, input().split())
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(int(num1/num2))
+print(num1%num2)
