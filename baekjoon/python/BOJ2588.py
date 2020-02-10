6a35a11ae9e487b4e813d0c3efa8573123ca53e2
ChoiGoun <gowoonvv@gmail.com>
2020-02-10 13:43:11 +0900

    Create BOJ2588.py

---
+++ baekjoon/python/BOJ2588.py
@@ -0,0 +1,13 @@
+"""
+baekjoon #2588 곱
+BOJ2588.py
+2020.02.10
+최고운 github.com/choigone
+"""
+
+num1 = int(input())
+num2 = input()
+print(int(num2[2])*num1)
+print(int(num2[1])*num1)
+print(int(num2[0])*num1)
+print(int(num2)*num1)
