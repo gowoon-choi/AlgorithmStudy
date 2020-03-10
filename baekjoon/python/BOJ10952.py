ea3fa0c17947a826bfdeda2c19ebda2cef32aa71
ChoiGoun <gowoonvv@gmail.com>
2020-03-10 19:12:27 +0900

    Create BOJ10952.py

---
+++ baekjoon/python/BOJ10952.py
@@ -0,0 +1,11 @@
+"""
+baekjoon #10952 A+B - 5
+BOJ10952.py
+2020.03.10
+최고운 github.com/choigone
+"""
+
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0: break
+    print(a+b)
