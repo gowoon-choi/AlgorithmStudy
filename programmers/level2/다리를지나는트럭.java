219c53052885ba95ca3620a18e4079e88fdb49f2
Gowoon Choi <gowoonvv@gmail.com>
2021-04-28 03:29:49 +0900

    Update 다리를지나는트럭.java

--- programmers/level2/다리를지나는트럭.java
+++ programmers/level2/다리를지나는트럭.java
@@ -21,22 +21,27 @@ public class Solution {
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 1;
         int current = 0;
+        int move;
         Vector<Truck> onBridge = new Vector<>();
         for (int i=0; i<truck_weights.length; i++) {
             if (onBridge.isEmpty() || current + truck_weights[i] <= weight){
                 onBridge.add(new Truck(truck_weights[i]));
                 current += truck_weights[i];
+                move = 1;
             }
-            else i--;
+            else {
+                i--;
+                move = bridge_length - onBridge.get(0).time;
+            }
             for (int j = 0; j < onBridge.size() ; j++) {
-                onBridge.get(j).time++;
+                onBridge.get(j).time+=move;
                 if (onBridge.get(j).time == bridge_length){
                     current -= onBridge.get(j).weight;
                     onBridge.remove(j);
                     j--;
                 }
             }
-            answer++;
+            answer+=move;
         }
         return answer+(bridge_length - onBridge.get(onBridge.size()-1).time);
     }
