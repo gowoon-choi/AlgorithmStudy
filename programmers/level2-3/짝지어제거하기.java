09ead9954d51b0184c15913121a0f7ff651e8d05
Gowoon Choi <gowoonvv@gmail.com>
2021-05-31 21:10:02 +0900

    Update 짝지어제거하기.java

--- programmers/level2-3/짝지어제거하기.java
+++ programmers/level2-3/짝지어제거하기.java
@@ -5,8 +5,7 @@ class Solution
     {
         char current;
         Stack<Character> stack = new Stack<>();
-        stack.add(s.charAt(0));
-        for(int i=1; i<s.length(); i++){
+        for(int i=0; i<s.length(); i++){
             current = s.charAt(i);
             if(!stack.isEmpty() && stack.peek() == current) stack.pop();
             else stack.push(current);
