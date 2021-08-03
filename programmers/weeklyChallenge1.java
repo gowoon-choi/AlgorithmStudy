5c8409515d91a771331c74a4eebd28c21b5c45f5
Gowoon Choi <gowoonvv@gmail.com>
2021-08-04 03:43:36 +0900

    Create weeklyChallenge1.java

---
+++ programmers/weeklyChallenge1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long sumTo(int num){
+        return num*(num+1)/2;
+    }
+    
+    public long solution(int price, int money, int count) {
+        long result = (sumTo(count)*price) - money;
+        if(result < 0) return -0;
+        else return result;
+    }
+}
