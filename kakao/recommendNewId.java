fc3afb98c68af31de2d9a6918a29eb3a19933483
Gowoon Choi <gowoonvv@gmail.com>
2021-08-24 09:26:43 +0900

    Update recommendNewId.java

--- kakao/recommendNewId.java
+++ kakao/recommendNewId.java
@@ -33,12 +33,9 @@ class Solution {
     }
 
     public static String rule4(String new_id){
-        while (new_id.length() > 0 && new_id.charAt(0) == '.') {
-            if(new_id.charAt(0) == '.') new_id = new_id.substring(1);
-        }
-        while (new_id.length() > 0 && new_id.charAt(new_id.length()-1) == '.'){
-            new_id = new_id.substring(0, new_id.length()-1);
-        }
+        if(new_id.charAt(0) == '.') new_id = new_id.substring(1);
+        if(new_id.length() > 0 && new_id.charAt(new_id.length()-1) == '.') new_id = new_id.substring(0, new_id.length()-1);
+        return new_id;
         return new_id;
     }
 
