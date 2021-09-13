1488fc6c25732de79c20fe2d16131c74d610caec
Gowoon Choi <gowoonvv@gmail.com>
2021-09-14 00:43:01 +0900

    Create 중복조합.java

---
+++ algorithm/중복조합.java
@@ -0,0 +1,21 @@
+package com.gowoon;
+
+public class AlgorithmStudy {
+    public static void combination(int[] arr, int[] out, int start, int depth, int r){
+        if(depth == r){
+            for(int num : out) System.out.print(num);
+            System.out.println();
+            return;
+        }
+        for(int i=start; i<arr.length; i++){
+            out[depth] = arr[i];
+            combination(arr, out, i, depth+1, r);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3};
+        int r = 2;
+        combination(arr, new int[r], 0, 0, r);
+    }
+}
