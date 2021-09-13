6282b9a66cadba8db41fb1113543efdc034fb632
Gowoon Choi <gowoonvv@gmail.com>
2021-09-14 00:11:54 +0900

    Create 중복순열.java

---
+++ algorithm/중복순열.java
@@ -0,0 +1,21 @@
+package com.gowoon;
+
+public class AlgorithmStudy {
+    public static void permutation(int[] arr, int[] out, int depth, int r){
+        if(depth == r){
+            for(int num: out) System.out.print(num);
+            System.out.println();
+            return;
+        }
+        for(int i=0; i<arr.length; i++){
+            out[depth] = arr[i];
+            permutation(arr, out, depth+1, r);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3};
+        int r = 2;
+        permutation(arr, new int[r], 0, r);
+    }
+}
